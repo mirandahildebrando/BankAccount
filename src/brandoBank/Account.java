c5b05bf40a66e36bf877c7096a4ec6a92a5c225a
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-02-01 16:49:37 -0300

    account withdrawal update

--- src/brandoBank/Account.java
+++ src/brandoBank/Account.java
@@ -5,5 +5,35 @@ public class Account {
 	private String ag;
 	private String cc;
 	private String name;
+	private double balance;
+	
+	static int MAX_LENGHT = 12;
+	
+	public Account(String ag, String cc, String name) {
+		
+		this.ag = ag;
+		this.cc = cc;
+		setName(name);
+	}
+	
+	public void setName(String name) {
+		if (name.length() > MAX_LENGHT) {
+			this.name = name.substring(0, MAX_LENGHT);
+		} else {
+			this.name = name;
+		}
+		System.out.println(this.name);
+	}
+	
+	public boolean withDraw(double value) {
+		if(balance < value) {
+			return false;
+		} else {
+		balance -= value;
+		System.out.println("valor a retirar: " + value + " , total na conta agora é " + balance);
+		return true;
+		}
+		
+	}
 
 }
--- src/brandoBank/App.java
+++ src/brandoBank/App.java
@@ -4,19 +4,22 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		//criar uma conta (agencia, conta, nome)
+		// limitar o nome = 12 caracteres
 		
 		System.out.println("Hello world");
 		
-		Account account = new Account(ag:"0001", cc:"1234", name:"Seu barriga");
+		Account account = new Account("0001", "1234", "Seu barriga");
 		
-		
-		//criar uma conta (agencia, conta, nome)
-		// limitar o nome = 12 caracteres
+		boolean succed = account.withDraw(200.0);
+		if(!succed) {
+			System.out.println("Você não tem saldo suficiente para sacar");
+		}
+
 		// sacar valores
 		// não pode sacar mais do que tem
 		// depositar 
 		// informar para o usuário as operações (saque, deposito)
 
 	}
-
 }
